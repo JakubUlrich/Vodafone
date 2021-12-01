54296f5b146a0296d6e5c6482a240ceaa569aefe
Jakub <kubik5@cenrtum.cz>
2021-12-01 12:15:27 +0100

    PRvní

---
+++ src/test/java/com/example/vodafone/MainPage.java
@@ -0,0 +1,13 @@
+package com.example.vodafone;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+// https://www.jetbrains.com/
+public class MainPage {
+    public SelenideElement seeAllToolsButton = $("a.wt-button_mode_primary");
+    public SelenideElement toolsMenu = $x("//div[contains(@class, 'menu-main__item') and text() = 'Developer Tools']");
+    public SelenideElement searchButton = $("[data-test='menu-main-icon-search']");
+}
