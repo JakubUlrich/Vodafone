9aeec2483c85d75fddd5467ed02368861271d7da
Jakub <kubik5@cenrtum.cz>
2021-12-01 16:39:12 +0100

    With great power comes great responsibility

--- src/test/java/com/example/vodafone/MainPage.java
+++ src/test/java/com/example/vodafone/MainPage.java
@@ -10,4 +10,6 @@ public class MainPage {
     public SelenideElement seeAllToolsButton = $("a.wt-button_mode_primary");
     public SelenideElement toolsMenu = $x("//div[contains(@class, 'menu-main__item') and text() = 'Developer Tools']");
     public SelenideElement searchButton = $("[data-test='menu-main-icon-search']");
+
+
 }
--- src/test/java/com/example/vodafone/MainPageTest.java
+++ src/test/java/com/example/vodafone/MainPageTest.java
@@ -1,23 +1,22 @@
 package com.example.vodafone;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selectors.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class MainPageTest {
     MainPage mainPage = new MainPage();
+    private String urlName = "https://www.vodafone.cz";
 
     @BeforeAll
     public static void setUpAll() {
@@ -26,47 +25,32 @@ public class MainPageTest {
     }
 
     @BeforeEach
-    public void setUp() {
-        //open("https://www.jetbrains.com/");
-    }
+    public void setUp() {}
 
     @Test
     public void Vodafone(){
-        open("https://www.vodafone.cz"); // Step 1
+        // Step 1
+        open(urlName);
         // Step 2
         $(By.className("vfcc__button")).click(); // Přijmutí cookies
         $(byText("Telefony a zařízení")).click();
-
         // Step 3
+        $(By.className("select2-container")).shouldHave(text("Nejsem"));
         // Step 4
+        $(By.className("tariffItem__name")).click();
+        sleep(10000);
         // Step 5
+        $(byText("Ne, chci telefon bez tarifu")).click();
+        sleep(10000);
         // Step 6
+        $(byText("Samsung Galaxy Z Flip3 5G 256 GB, černá")).click();
         // Step 7
+        $(byText("Koupit telefon bez tarifu")).click();
+        // Step 8
+        String pngFileName = screenshot("my_file_name");
+        // Step 9
+
+
+        sleep(10000);
     }
-  /*
-    @Test
-    public void search() {
-        mainPage.searchButton.click();
-
-        $("[data-test='search-input']").sendKeys("Selenium");
-        $("button[data-test='full-search-button']").click();
-
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
-    }
-
- @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.hover();
-
-        $("div[data-test='menu-main-popup-content']").shouldBe(visible);
-    }
-
-    @Test
-    public void navigationToAllTools() {
-        mainPage.seeAllToolsButton.click();
-
-        $("#products-page").shouldBe(visible);
-
-        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
-    }*/
 }
